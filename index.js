0541a28445965c3b20d88de5833c065cdd887dbe
Jim Aman <jim.a.aman@gmail.com>
2019-08-24 19:18:03 -0500

    pokemon appear and can try to be caught

--- index.js
+++ index.js
@@ -25,23 +25,89 @@ const options = {
   channels: ["scasplte2"]
 };
 
-// initialize database and functions
-// Set default state
-db.defaults({ items: [] }).write();
+// Set default database state
+db.defaults({ pokemon: [], user: {}, catchablePokemon: "" }).write();
 
-function add() {
-  db.get("items")
-    .push({ time: Date.now() })
-    .write();
+// Define functions
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
-function reset() {
-  db.set("items", []).write();
+function readPoke(pokeID) {
+  const poke = db
+    .get("pokemon")
+    .find({ id: pokeID })
+    .value();
+  return poke.name;
+  //   return (str = JSON.stringify(poke.name, null, 2));
 }
 
-function read() {
-  const state = db.getState();
-  return (str = JSON.stringify(state, null, 2));
+function getUser(username) {
+    return db
+      .get("user")
+      .find({ name: username })
+      .value();
+  }
+  
+  function isUserInDB(username) {
+    let existingUser = false;
+    if (getUser(username)) {
+      existingUser = true;
+    }
+    return existingUser;
+  }
+  
+  function newUser(username) {
+    db.get("user")
+      .push({{ name: username, pokemon: [] }})
+      .write();
+  }
+  
+function updateUser() {}
+
+function updateCatchablePokemon(wildPokemon) {
+  db.set("catchablePokemon", wildPokemon).write();
+}
+
+// function to check if the user is registered and to attempt catch if so
+function allowAttempt(username) {
+  let msg = "";
+  if (isUserInDB(username)) {
+    msg = attemptCatch(username);
+  } else {
+    msg = `Sorry ${username}, please visit Professor Oak ( !visitprofoak ) to register your Pokedex before trying to catch Pokemon.`;
+  }
+  return msg;
+}
+
+// function to compute the catch attempt
+function attemptCatch(username) {
+  // Retrieve catchable pokemon for convenience
+  const wildPK = db.get("catchablePokemon").value();
+  // calculate normalization (makes catching more variable)
+  //const normalization = getRandomInt(12);
+  const normalization = 1;
+  // calculate attempt
+  const attempt = getRandomInt(100);
+  // check if the pokemon was caught
+  let msg = "";
+  if (wildPK) {
+    if (attempt < 100 / normalization) {
+      msg = `Congratulations ${username}! You caught a ${wildPK}.`;
+      updateUser(username);
+      const poke = db
+        .get("pokemon")
+        .find({ name: wildPK })
+        .value();
+      console.log(poke);
+    } else {
+      updateCatchablePokemon("");
+      msg = `Your pokeball missed ${username}! You scared away the ${wildPK}.`;
+    }
+  } else {
+    msg = `Sorry ${username}. There are currently no wild pokemon around to catch.`;
+  }
+  return msg;
 }
 
 // initialize chat client
@@ -56,31 +122,70 @@ client.on("connected", (address, port) => {
 });
 
 client.on("chat", (channel, user, message, self) => {
-  console.log(user);
-  if (message === "!test") {
-    client.action("scasplte2", "Testing whether a command can work!");
-  }
+  switch (message) {
+    case "!pokeball":
+      client.action("scasplte2", allowAttempt(user.username));
+      break;
 
-  if (user["display-name"] === "scasplte2") {
-    client.action(
-      "scasplte2",
-      `Hello scasplte2! I hear you are talking about me?`
-    );
-  }
+    case "!visitprofoak":
+      newUser(user["display-name"]);
+      client.action(
+        "scasplte2",
+        `Thanks for registering your Pokedex ${
+          user["display-name"]
+        }! You can view it's contents using !pokedex`
+      );
+      console.log(getUser(user["display-name"]));
+      break;
 
-  if (message === "!add") {
-    add();
-  }
+    case "!pokedex":
+      console.log(
+        db
+          .get("user")
+          .find({ name: user["display-name"] })
+          .value()
+      );
+      client.action("scasplte2", ``);
+      break;
 
-  if (message === "!reset") {
-    reset();
+    // A wild pokemon will appear if a chat message has been sent within 1 second of a 15 minute interval
+    default:
+      //if (Date.now() % (0.2 * 60 * 1000) < 1000) {
+      if (user["display-name"] === "scasplte2") {
+        const wildPoke = readPoke(getRandomInt(151));
+        updateCatchablePokemon(wildPoke);
+        client.action("scasplte2", `A wild ${wildPoke} appeared!`);
+      }
   }
-
-  // TODO: adding ACTION before the message in IRC chat for some reason
-  if (message === "!read") {
-    console.log(read());
-    client.action("scasplte2", `The new db state is: ${read()}`);
-  }
-
-  client.action("scasplte2", `Hello ${user["display-name"]}!`);
 });
+
+// function add() {
+//     db.get('items')
+//       .push({ time: Date.now() })
+//       .write()
+//   }
+// function read() {
+//   const state = db.getState();
+//   return (str = JSON.stringify(state, null, 2));
+// }
+// function reset() {
+//   db.set("items", []).write();
+// }
+
+//   if (user["display-name"] === "scasplte2") {
+//     client.action(
+//       "scasplte2",
+//       `Hello scasplte2! I hear you are talking about me?`
+//     );
+//   }
+//   if (message === "!add") {
+//     add();
+//   }
+//   if (message === "!reset") {
+//     reset();
+//   }
+// TODO: adding ACTION before the message in IRC chat for some reason
+//   if (message === "!read") {
+//     client.action("scasplte2", `The new db state is: ${read()}`);
+//   }
+//   client.action("scasplte2", `Hello ${user["display-name"]}!`);
